a9cf461a918880df63ea05fe12312078c2bf56c9
DUASAMANA <161742251+DUASAMANA@users.noreply.github.com>
2024-06-29 21:46:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 // Set up storage with multer
 const storage = multer.diskStorage({
 destination: (req, file, cb) => {
-cb(null, 'uploads/');
+cb(null, path.join(__dirname,'uploads/');
 },
 filename: (req, file, cb) => {
 cb(null, Date.now() + path.extname(file.originalname));
